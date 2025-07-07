ae6310a079a69f1451e45ca37a929f25d072969d
cloud-ru-tech-robot <cloud-ru-tech-robot>
2025-07-07 15:52:00 +0000

    new storybook version

---
+++ snack-uikit/assets/FieldStepper.story-DLRo3A6Q.js
@@ -0,0 +1,47 @@
+import{j as r}from"./jsx-runtime-BTJTZTIL.js";import{r as a}from"./index-CJ_LmFaV.js";import{c as n,a as c,b as f,C as u,P as g,s as x}from"./styles.module-Cav8qwp7.js";import{F as l}from"./FieldStepper-BX0Tyu2S.js";import"./index-ChsGqxH_.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-DgzWz_a-.js";import"./Sprite-DoNyyhE0.js";import"./sprite.symbol-DpN2ERsh.js";import"./index-CAfvFIBF.js";import"./index-BAKcRSlX.js";import"./index-DNCO-vcx.js";import"./index-Y3TRpJ-L.js";import"./index-DrT0s6v0.js";import"./index-Ca89sW9R.js";import"./index-C3mvkShn.js";import"./index-CGBRtoFy.js";import"./index-BGTKE3tV.js";import"./index-DHAmGKAZ.js";import"./index-h0kOFyIh.js";import"./index-BfUMklLA.js";import"./index-B0xwoRpp.js";import"./index-B2h5DBoi.js";import"./index-5mF7KA5g.js";import"./index-WpT-B0mJ.js";import"./index-B_69vUNX.js";import"./index-B80hK4m0.js";import"./index-w0WAZL2A.js";import"./index-Dn6vuaYJ.js";import"./index-CTTgWa-s.js";import"./index-CF_OgQNJ.js";import"./index-DBTBMFOQ.js";import"./index-CXJdfWGn.js";import"./index-x12pshUM.js";import"./index-CGSydWJd.js";import"./index-DbF-M2Rm.js";import"./FieldDecorator-Di5fhH_4.js";import"./index-DUmpqB7p.js";import"./constants-9uwKbtvo.js";import"./TruncateString-CGm6SssY.js";import"./useIsomorphicLayoutEffect-BAXQmlEq.js";import"./isBrowser-C2N6XvuH.js";import"./componentPropsProcessors-UJEZy8aQ.js";import"./Tooltip-BRX_-OEr.js";import"./PopoverPrivate-NoUVbAMG.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./index-JZGyZbA2.js";import"./index-B7wZyJkK.js";import"./LocaleProvider-CyhbxqE1.js";import"./useValueControl-CNLw7ht-.js";import"./usePrefix-BmEKZSSL.js";import"./FieldContainerPrivate-B_xJK31Q.js";import"./InputPrivate-iuTKGpC7.js";import"./ButtonFunction-BJzRx2eA.js";import"./utils-D4P0b1iJ.js";import"./Counter-dO15w468.js";import"./Sun-CjKF0Bsh.js";import"./constants-BAH7I9kc.js";const Ce={title:"Components/Fields/Field Stepper",component:l},b=({size:i,...e})=>{const[d,o]=a.useState(e.value);return a.useEffect(()=>{o(e.value)},[e.value]),r.jsx("div",{className:x.wrapper,"data-size":i,children:r.jsx(l,{...e,size:i,value:d,onChange:o})})},t={render:b,args:{id:"text",value:6,min:0,max:10,step:1,readonly:!1,disabled:!1,label:"Label text",labelTooltip:"Tooltip description",required:!1,caption:"Caption",hint:"Hint text",size:"s",prefix:"",postfix:"",validationState:"default",allowMoreThanLimits:!0},argTypes:{...g,...u,localeName:{table:{disable:!0}},prefixIcon:{table:{disable:!0}}},parameters:{readme:{sidebar:[`Latest version: ${n.version}`,c,f]},packageName:n.name,design:{name:"Figma",type:"figma",url:"https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-2.0.0?type=design&node-id=41%3A38747&mode=design&t=8dDi5X6Lfgs6Cxji-1"}}};var p,m,s;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    id: 'text',
+    value: 6,
+    min: 0,
+    max: 10,
+    step: 1,
+    readonly: false,
+    disabled: false,
+    label: 'Label text',
+    labelTooltip: 'Tooltip description',
+    required: false,
+    caption: 'Caption',
+    hint: 'Hint text',
+    size: 's',
+    prefix: '',
+    postfix: '',
+    validationState: 'default',
+    allowMoreThanLimits: true
+  },
+  argTypes: {
+    ...PREFIX_POSTFIX_ARG_TYPES,
+    ...COMMON_ARG_TYPES,
+    localeName: {
+      table: {
+        disable: true
+      }
+    },
+    prefixIcon: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  parameters: {
+    readme: {
+      sidebar: [\`Latest version: \${componentPackage.version}\`, componentReadme, componentChangelog]
+    },
+    packageName: componentPackage.name,
+    design: {
+      name: 'Figma',
+      type: 'figma',
+      url: 'https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-2.0.0?type=design&node-id=41%3A38747&mode=design&t=8dDi5X6Lfgs6Cxji-1'
+    }
+  }
+}`,...(s=(m=t.parameters)==null?void 0:m.docs)==null?void 0:s.source}}};export{Ce as default,t as fieldStepper};
