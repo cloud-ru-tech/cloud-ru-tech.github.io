b61a32d93256119467f5a7d0350f2a3dccf8d9e4
cloud-ru-tech-robot <cloud-ru-tech-robot>
2025-07-10 07:18:32 +0000

    new storybook version

---
+++ snack-uikit/assets/FieldDecorator.story-D1fb_Wkc.js
@@ -0,0 +1,29 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{c as i,a as l,b as d,C as a,s as r}from"./styles.module-BksNF9XV.js";import{F as s}from"./FieldDecorator-CxilPKFM.js";import{T as c}from"./Typography-DmSN6cv6.js";import"./index-ChsGqxH_.js";import"./index-DgzWz_a-.js";import"./Sprite-DoNyyhE0.js";import"./index-CJ_LmFaV.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./sprite.symbol-DpN2ERsh.js";import"./index-CAfvFIBF.js";import"./index-BAKcRSlX.js";import"./index-DNCO-vcx.js";import"./index-Y3TRpJ-L.js";import"./index-DrT0s6v0.js";import"./index-Ca89sW9R.js";import"./index-C3mvkShn.js";import"./index-CGBRtoFy.js";import"./index-BGTKE3tV.js";import"./index-DHAmGKAZ.js";import"./index-h0kOFyIh.js";import"./index-BfUMklLA.js";import"./index-B0xwoRpp.js";import"./index-B2h5DBoi.js";import"./index-5mF7KA5g.js";import"./index-WpT-B0mJ.js";import"./index-B_69vUNX.js";import"./index-B80hK4m0.js";import"./index-w0WAZL2A.js";import"./index-Dn6vuaYJ.js";import"./index-CTTgWa-s.js";import"./index-CF_OgQNJ.js";import"./index-DBTBMFOQ.js";import"./index-CXJdfWGn.js";import"./index-x12pshUM.js";import"./index-CGSydWJd.js";import"./index-DbF-M2Rm.js";import"./index-DUmpqB7p.js";import"./constants-9uwKbtvo.js";import"./TruncateString--U_6xn2L.js";import"./useIsomorphicLayoutEffect-BAXQmlEq.js";import"./isBrowser-C2N6XvuH.js";import"./componentPropsProcessors-UJEZy8aQ.js";import"./Tooltip-DGvBTeR0.js";import"./PopoverPrivate-GlQeSlaK.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./index-JZGyZbA2.js";const mt={title:"Components/Fields/Field Decorator",component:s},g=o=>t.jsx("div",{className:r.wrapper,"data-size":o.size,children:t.jsx(s,{...o,children:t.jsx("div",{className:r.decoratorStory,children:t.jsx(c.SansBodyM,{children:"Custom content - ReactNode"})})})}),e={render:g,args:{label:"Label text",labelTooltip:"Tooltip description",required:!1,caption:"Caption",hint:"Hint text",size:"s",readonly:!1,validationState:"default",disabled:!1},argTypes:{validationState:a.validationState,labelTooltip:a.labelTooltip},parameters:{readme:{sidebar:[`Latest version: ${i.version}`,l,d]},packageName:i.name,design:{name:"Figma",type:"figma",url:"https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-2.0.0?type=design&node-id=41%3A38747&mode=design&t=8dDi5X6Lfgs6Cxji-1"}}};var n,p,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: 'Label text',
+    labelTooltip: 'Tooltip description',
+    required: false,
+    caption: 'Caption',
+    hint: 'Hint text',
+    size: 's',
+    readonly: false,
+    validationState: 'default',
+    disabled: false
+  },
+  argTypes: {
+    validationState: COMMON_ARG_TYPES.validationState,
+    labelTooltip: COMMON_ARG_TYPES.labelTooltip
+  },
+  parameters: {
+    readme: {
+      sidebar: [\`Latest version: \${componentPackage.version}\`, componentReadme, componentChangelog]
+    },
+    packageName: componentPackage.name,
+    design: {
+      name: 'Figma',
+      type: 'figma',
+      url: 'https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-2.0.0?type=design&node-id=41%3A38747&mode=design&t=8dDi5X6Lfgs6Cxji-1'
+    }
+  }
+}`,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};export{mt as default,e as fieldDecorator};
---
+++ snack-uikit/assets/TimePicker.story-C61KSNrZ.js
@@ -0,0 +1,107 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{c as T}from"./index-DUmpqB7p.js";import{r as u}from"./index-CJ_LmFaV.js";import{c as g,a as M,b as N,s as r}from"./styles.module-DfUEL3Qi.js";import{S as a}from"./TimePickerBase-B5NG6yuh.js";import{T as k}from"./TimePicker-DOSLQ4Xg.js";import{S as L}from"./Scroll-B3e6J9cA.js";import"./index-ChsGqxH_.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./hooks-DgdbwEdu.js";import"./componentPropsProcessors-UJEZy8aQ.js";import"./useValueControl-C51frgGe.js";import"./index-JZGyZbA2.js";import"./Checkbox-qiHN918A.js";import"./index-B7wZyJkK.js";import"./constants-BObEONKm.js";import"./TogglePrivate-BTSew7b5.js";import"./index-w0WAZL2A.js";import"./index-Y3TRpJ-L.js";import"./TruncateString--U_6xn2L.js";import"./useIsomorphicLayoutEffect-BAXQmlEq.js";import"./isBrowser-C2N6XvuH.js";import"./Tooltip-DGvBTeR0.js";import"./PopoverPrivate-GlQeSlaK.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./Switch-DZam4yEg.js";import"./index-DHAmGKAZ.js";import"./index-CGBRtoFy.js";import"./index-DrT0s6v0.js";import"./Dropdown-C18FqxyO.js";import"./index-C3mvkShn.js";import"./index-CXJdfWGn.js";import"./LocaleProvider-CyhbxqE1.js";import"./index-3-msAVFO.js";import"./IconPredefined-yX8Rdo-2.js";import"./Typography-DmSN6cv6.js";import"./ButtonTonal-hNy0z_PL.js";import"./utils-DdRMp2qm.js";import"./Counter-dO15w468.js";import"./Sun-CjKF0Bsh.js";import"./constants-BAH7I9kc.js";import"./ButtonFilled-CmFbPc0T.js";import"./Divider-C1X9zrkx.js";import"./Spinner-PYn9r2mo.js";import"./ButtonFunction-BcmYEDa9.js";import"./useButtonNavigation-DNIYWp-y.js";import"./useClearButton-DCno6QJq.js";import"./constants-9uwKbtvo.js";import"./InputPrivate-ATG-mTzh.js";import"./List-TyK0rSGt.js";const Le={title:"Components/Calendar/Time Picker",component:k},_={[a.S]:r.scroll_timepicker_s,[a.M]:r.scroll_timepicker_m,[a.L]:r.scroll_timepicker_l},E=({todayHours:d,todayMinutes:c,todaySeconds:b,valueHours:m,valueMinutes:s,valueSeconds:l,...o})=>{const p=u.useMemo(()=>[m,s,l].every(n=>n===void 0)?void 0:{hours:m,minutes:s,seconds:l},[m,s,l]),[e,y]=u.useState(p);u.useEffect(()=>{y(p)},[p]);const C=n=>{var v;y(n),(v=o.onChangeValue)==null||v.call(o,n)},x=[d,c,b].every(n=>n===void 0)?void 0:new Date(0,0,0,d,c,b),P={...o,today:x,value:e,onChangeValue:C},j=String(e==null?void 0:e.hours).padStart(2,"0"),w=String(e==null?void 0:e.minutes).padStart(2,"0"),F=String(e==null?void 0:e.seconds).padStart(2,"0");return t.jsxs(t.Fragment,{children:[t.jsx("div",{className:T(r.story,_[o.size||a.M]),children:t.jsx(L,{children:t.jsx(k,{...P})})}),t.jsx("div",{className:r.valueHolder,"data-test-id":"timepicker-value-holder",children:e?`${j}:${w}:${F}`:""})]})},i={render:E,args:{showSeconds:!0,size:a.M,fitToContainer:!0},argTypes:{value:{table:{disable:!0}},navigationStartRef:{table:{disable:!0}},onChangeValue:{table:{disable:!0}},onFocusLeave:{table:{disable:!0}},buildCellProps:{table:{disable:!0}},style:{table:{disable:!0}},defaultValue:{table:{disable:!0}},locale:{table:{disable:!0}},className:{table:{disable:!0}},today:{table:{disable:!0}},todayHours:{name:"[today] hours",control:{type:"number"}},todayMinutes:{name:"[today] minutes",control:{type:"number"}},todaySeconds:{name:"[today] seconds",control:{type:"number"}},valueHours:{name:"[value] hours",control:{type:"number"}},valueMinutes:{name:"[value] minutes",control:{type:"number"}},valueSeconds:{name:"[value] seconds",control:{type:"number"}}},parameters:{readme:{sidebar:[`Latest version: ${g.version}`,M,N]},packageName:g.name,design:{name:"Figma",type:"figma",url:"https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A27244&mode=design"}}};var S,f,h;i.parameters={...i.parameters,docs:{...(S=i.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    showSeconds: true,
+    size: SIZE.M,
+    fitToContainer: true
+  },
+  argTypes: {
+    value: {
+      table: {
+        disable: true
+      }
+    },
+    navigationStartRef: {
+      table: {
+        disable: true
+      }
+    },
+    onChangeValue: {
+      table: {
+        disable: true
+      }
+    },
+    onFocusLeave: {
+      table: {
+        disable: true
+      }
+    },
+    buildCellProps: {
+      table: {
+        disable: true
+      }
+    },
+    style: {
+      table: {
+        disable: true
+      }
+    },
+    defaultValue: {
+      table: {
+        disable: true
+      }
+    },
+    locale: {
+      table: {
+        disable: true
+      }
+    },
+    className: {
+      table: {
+        disable: true
+      }
+    },
+    today: {
+      table: {
+        disable: true
+      }
+    },
+    todayHours: {
+      name: '[today] hours',
+      control: {
+        type: 'number'
+      }
+    },
+    todayMinutes: {
+      name: '[today] minutes',
+      control: {
+        type: 'number'
+      }
+    },
+    todaySeconds: {
+      name: '[today] seconds',
+      control: {
+        type: 'number'
+      }
+    },
+    valueHours: {
+      name: '[value] hours',
+      control: {
+        type: 'number'
+      }
+    },
+    valueMinutes: {
+      name: '[value] minutes',
+      control: {
+        type: 'number'
+      }
+    },
+    valueSeconds: {
+      name: '[value] seconds',
+      control: {
+        type: 'number'
+      }
+    }
+  },
+  parameters: {
+    readme: {
+      sidebar: [\`Latest version: \${componentPackage.version}\`, componentReadme, componentChangelog]
+    },
+    packageName: componentPackage.name,
+    design: {
+      name: 'Figma',
+      type: 'figma',
+      url: 'https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A27244&mode=design'
+    }
+  }
+}`,...(h=(f=i.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};export{Le as default,i as timePicker};
