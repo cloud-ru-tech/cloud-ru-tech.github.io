630fe2da6f4187312d9f242ae9f6e214ddd2c87f
cloud-ru-tech-robot <cloud-ru-tech-robot>
2025-07-15 06:04:23 +0000

    new storybook version

---
+++ snack-uikit/assets/ChipChoiceRow.story-C8gAp6EM.js
@@ -0,0 +1,40 @@
+import{j as e}from"./jsx-runtime-BTJTZTIL.js";import{r as o}from"./index-CJ_LmFaV.js";import{c as p,a as g,b as h,s as C}from"./styles.module-BWbuBTdq.js";import{C as d,a as S}from"./ChipChoiceRow-BvOD0RHG.js";import{D as y,f as V,S as w}from"./helpers-DSWmnuo9.js";import"./index-ChsGqxH_.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-DUmpqB7p.js";import"./index-JZGyZbA2.js";import"./constants-BIigMBdG.js";import"./index-BPh-CSIf.js";import"./FuzzySearch-C8qpdhmO.js";import"./LocaleProvider-CyhbxqE1.js";import"./ButtonFunction-DKB0tYFA.js";import"./utils-CWsIvcya.js";import"./Counter-dO15w468.js";import"./componentPropsProcessors-UJEZy8aQ.js";import"./Sun-Bft0ZijA.js";import"./constants-BAH7I9kc.js";import"./ButtonFilled-B6cD5uG0.js";import"./index-B7wZyJkK.js";import"./index-BQAaeyfQ.js";import"./TruncateString-BPmgFnBz.js";import"./useIsomorphicLayoutEffect-BAXQmlEq.js";import"./isBrowser-C2N6XvuH.js";import"./Tooltip-BNp3usZT.js";import"./PopoverPrivate-CnKgsFAm.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./useValueControl-C51frgGe.js";import"./Dropdown-uG1Hplv7.js";import"./hooks-BtklQH5r.js";import"./Checkbox-BJ1fNH24.js";import"./constants-BObEONKm.js";import"./TogglePrivate-BTSew7b5.js";import"./index-3XDw48l5.js";import"./index-DtRrUciF.js";import"./Switch-DmwOlfxh.js";import"./index-C7IVzcIX.js";import"./index-BLj3vrxU.js";import"./index-DLGJiw2b.js";import"./index-Baw9oej9.js";import"./index-C9BZUUlt.js";import"./IconPredefined-yX8Rdo-2.js";import"./Typography-DmSN6cv6.js";import"./ButtonTonal-B1791CGe.js";import"./Divider-C1X9zrkx.js";import"./Spinner-BoRMrYYR.js";import"./Scroll-B3e6J9cA.js";import"./useButtonNavigation-CVCsehRW.js";import"./useClearButton-DMWmUR1d.js";import"./constants-9uwKbtvo.js";import"./InputPrivate-DYeeBMjM.js";import"./DropList-BZNescUm.js";import"./Calendar-B8yxAtRi.js";import"./TimePickerBase-DrOptWqJ.js";import"./List-cui97_1M.js";import"./TimePicker-Kvfhcdd2.js";import"./index-CwHIobxP.js";const Oe={title:"Components/Chips/ChipChoiceRow",component:d};function k({useDefaultValues:i,...a}){const t=o.useMemo(()=>i?a.defaultValue:{},[a.defaultValue,i]),[s,n]=o.useState(t),[f,m]=o.useState(Object.keys(s??{}));return o.useLayoutEffect(()=>{n(t),m(Object.keys(t??{}))},[t,i]),e.jsxs(e.Fragment,{children:[e.jsx("div",{className:C.chipChoiceRowWrapper,children:e.jsx(d,{...a,value:s,onChange:n,visibleFilters:f,onVisibleFiltersChange:m})}),e.jsx("span",{style:{opacity:0},"data-test-id":w.State,children:JSON.stringify(s)})]})}const r={render:k,args:{size:"s",filters:V,showClearButton:!0,showAddButton:!0,useDefaultValues:!0,defaultValue:y},argTypes:{size:{options:Object.values(S),control:{type:"radio"}},useDefaultValues:{name:"[Story] use default values",type:"boolean"},defaultValue:{if:{arg:"useDefaultValues",eq:!0}}},parameters:{readme:{sidebar:[`Latest version: ${p.version}`,g,h]},packageName:p.name,design:{name:"Figma",type:"figma",url:"https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A152236&mode=design"}}};var l,u,c;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    size: 's',
+    filters: filtersMock,
+    showClearButton: true,
+    showAddButton: true,
+    useDefaultValues: true,
+    defaultValue: DEFAULT_VALUES as Filters
+  },
+  argTypes: {
+    size: {
+      options: Object.values(CHIP_CHOICE_ROW_SIZE),
+      control: {
+        type: 'radio'
+      }
+    },
+    useDefaultValues: {
+      name: '[Story] use default values',
+      type: 'boolean'
+    },
+    defaultValue: {
+      if: {
+        arg: 'useDefaultValues',
+        eq: true
+      }
+    }
+  },
+  parameters: {
+    readme: {
+      sidebar: [\`Latest version: \${componentPackage.version}\`, componentReadme, componentChangelog]
+    },
+    packageName: componentPackage.name,
+    design: {
+      name: 'Figma',
+      type: 'figma',
+      url: 'https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A152236&mode=design'
+    }
+  }
+}`,...(c=(u=r.parameters)==null?void 0:u.docs)==null?void 0:c.source}}};export{r as chipChoiceRow,Oe as default};
