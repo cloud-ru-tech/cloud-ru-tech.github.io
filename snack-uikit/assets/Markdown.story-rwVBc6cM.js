93e95431ea932bb22bb23e8349eac428f4219065
cloud-ru-tech-robot <cloud-ru-tech-robot>
2025-07-23 14:48:36 +0000

    new storybook version

---
+++ snack-uikit/assets/Markdown.story-rwVBc6cM.js
@@ -0,0 +1,18 @@
+import{j as r}from"./jsx-runtime-BTJTZTIL.js";import{t as n}from"./helpers-BM83PUJX.js";import{M as i,c as e,a as s,b as c,d,s as g}from"./styles.module-DnbTsvdf.js";import"./index-ChsGqxH_.js";import"./index-Bxu_udwM.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-DdXvKv-Q.js";import"./index-CJ_LmFaV.js";import"./index-DUmpqB7p.js";import"./index-Chjiymov.js";import"./LocaleProvider-CyhbxqE1.js";import"./index-C7IVzcIX.js";import"./index-BLj3vrxU.js";import"./index-BQAaeyfQ.js";import"./Scroll-B3e6J9cA.js";import"./useIsomorphicLayoutEffect-BAXQmlEq.js";import"./isBrowser-C2N6XvuH.js";import"./ProgressBar-DoG3l5ps.js";import"./componentPropsProcessors-UJEZy8aQ.js";import"./index-6PN_JoHT.js";import"./index-C-NxZ9RO.js";import"./index-zbp_5bxn.js";import"./TruncateString-D12I7bk8.js";import"./Tooltip-BNp3usZT.js";import"./PopoverPrivate-CnKgsFAm.js";import"./index-JZGyZbA2.js";import"./Link-DxTCR0Rb.js";import"./Sun-CjKF0Bsh.js";import"./constants-BAH7I9kc.js";import"./react-markdown-DttXII6_.js";import"./Divider-C1X9zrkx.js";import"./CodeEditor-Blt-8sbr.js";import"./index-DTH25FFD.js";import"./index-DtRrUciF.js";import"./index-CX1lIXbQ.js";import"./ButtonFunction-BElZbry1.js";import"./utils-BM2s5Pk-.js";import"./Counter-dO15w468.js";import"./Typography-DmSN6cv6.js";import"./Spinner-PYn9r2mo.js";const oo={title:"Components/Markdown/Markdown",component:i},l=({...p})=>r.jsx("div",{className:g.wrapper,children:r.jsx(i,{...p,onCopyClick:()=>n.userAction.success({label:"Скопировано"})})}),o={render:l,args:{value:d},argTypes:{},parameters:{readme:{sidebar:[`Latest version: ${e.version}`,s,c]},packageName:e.name,design:{name:"Figma",type:"figma",url:"https://www.figma.com/design/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-5.0.0?node-id=1444-818"}}};var t,m,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    value: MARKDOWN
+  },
+  argTypes: {},
+  parameters: {
+    readme: {
+      sidebar: [\`Latest version: \${componentPackage.version}\`, componentReadme, componentChangelog]
+    },
+    packageName: componentPackage.name,
+    design: {
+      name: 'Figma',
+      type: 'figma',
+      url: 'https://www.figma.com/design/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-5.0.0?node-id=1444-818'
+    }
+  }
+}`,...(a=(m=o.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};export{oo as default,o as markdown};
