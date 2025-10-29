dd93add5b79c1563615873d5553c26d2e904e99b
cloud-ru-tech-robot <cloud-ru-tech-robot>
2025-10-29 11:39:11 +0000

    new storybook version

---
+++ snack-uikit/assets/ChipChoiceSingle.story-mUiJwIiP.js
@@ -0,0 +1,26 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{c as r,a as S,b as O,s as f}from"./styles.module-il507PaD.js";import{C as I}from"./ChipChoiceStoryWrap-DjqtuFyE.js";import{C as h,a as _,B as i,F as P}from"./constants-CrY15HQF.js";import{C as a}from"./index-pfp2-1lG.js";import"./index-ChsGqxH_.js";import"./index-CJ_LmFaV.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./constants-C4fmyMeh.js";import"./constants-QC2nOzBj.js";import"./index-BtuvvjWr.js";import"./Sprite-DoNyyhE0.js";import"./sprite.symbol-DpN2ERsh.js";import"./index-CAfvFIBF.js";import"./index-BAKcRSlX.js";import"./index-DNCO-vcx.js";import"./index-Y3TRpJ-L.js";import"./index-DreyRvUE.js";import"./index-Ca89sW9R.js";import"./index-C3mvkShn.js";import"./index-BGTKE3tV.js";import"./index-DHAmGKAZ.js";import"./index-h0kOFyIh.js";import"./index-BfUMklLA.js";import"./index-B0xwoRpp.js";import"./index-B2h5DBoi.js";import"./index-5mF7KA5g.js";import"./index-WpT-B0mJ.js";import"./index-B_69vUNX.js";import"./index-B80hK4m0.js";import"./index-w0WAZL2A.js";import"./index-Dn6vuaYJ.js";import"./index-CTTgWa-s.js";import"./index-CF_OgQNJ.js";import"./index-DBTBMFOQ.js";import"./index-CXJdfWGn.js";import"./index-x12pshUM.js";import"./index-CGSydWJd.js";import"./index-DbF-M2Rm.js";import"./Tag-Dyz5wRNP.js";import"./index-DUmpqB7p.js";import"./constants-BaREPXw5.js";import"./componentPropsProcessors-UJEZy8aQ.js";import"./Tooltip-DzAF_V2U.js";import"./PopoverPrivate-CnKgsFAm.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./index-JZGyZbA2.js";import"./isBrowser-C2N6XvuH.js";import"./useIsomorphicLayoutEffect-BAXQmlEq.js";import"./FuzzySearch-C8qpdhmO.js";import"./LocaleProvider-B4VrUkyw.js";import"./ButtonFunction-DO8-q7Gq.js";import"./utils-CWv1hTMo.js";import"./Counter-DXWSEFY6.js";import"./Sun-CjKF0Bsh.js";import"./constants-BAH7I9kc.js";import"./ButtonFilled-pGiP3SQa.js";import"./index-B7wZyJkK.js";import"./TruncateString-BMtbUp3O.js";import"./useValueControl-CNLw7ht-.js";import"./Dropdown-4YXb2YvP.js";import"./hooks-DON7lT9t.js";import"./Checkbox-B-86ZbLV.js";import"./constants-BObEONKm.js";import"./TogglePrivate-BTSew7b5.js";import"./Switch-Cq7kjPHZ.js";import"./index-BhVwnnbR.js";import"./IconPredefined-DVxQYij_.js";import"./Typography-COVcnQ5F.js";import"./ButtonTonal-BOArSFPu.js";import"./Divider-C1X9zrkx.js";import"./Scroll-CyeY1SIb.js";import"./Spinner-PYn9r2mo.js";import"./useButtonNavigation-CVCsehRW.js";import"./useClearButton-CAlOraVE.js";import"./constants-9uwKbtvo.js";import"./InputPrivate-alc9Nisi.js";import"./DropList-BeTwO9JS.js";import"./Calendar-BYT_K998.js";import"./TimePickerBase-Cd5N2wV3.js";import"./List-Ddt6g5S5.js";import"./TimePicker-CxvY6Erf.js";const ot={title:"Components/Chips/ChipChoice",component:a.Single},T=({useDefaultValue:n,useBaseOptions:s,showClickCounter:c,showClearButton:l,defaultValue:d,...C})=>t.jsx(I,{showClickCounter:c,showClearButton:l,defaultValue:n?d||i[0].value:void 0,chipControlled:({increaseCounter:g,...u})=>t.jsx(a.Single,{...C,...u,options:s?i:P,onClick:g,dropDownClassName:f.droplist})}),o={render:T,args:{..._,useBaseOptions:!1,scrollToSelectedItem:!1,autoApply:!0},argTypes:{...h,useBaseOptions:{name:"[Stories]: BaseOptions"}},parameters:{readme:{sidebar:[`Latest version: ${r.version}`,S,O]},packageName:r.name,design:{name:"Figma",type:"figma",url:"https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A148671&mode=design"}}};var m,p,e;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    ...CHIP_CHOICE_STORY_ARGS,
+    useBaseOptions: false,
+    scrollToSelectedItem: false,
+    autoApply: true
+  },
+  argTypes: {
+    ...CHIP_CHOICE_ARG_TYPES,
+    useBaseOptions: {
+      name: '[Stories]: BaseOptions'
+    }
+  },
+  parameters: {
+    readme: {
+      sidebar: [\`Latest version: \${componentPackage.version}\`, componentReadme, componentChangelog]
+    },
+    packageName: componentPackage.name,
+    design: {
+      name: 'Figma',
+      type: 'figma',
+      url: 'https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A148671&mode=design'
+    }
+  }
+}`,...(e=(p=o.parameters)==null?void 0:p.docs)==null?void 0:e.source}}};export{o as chipChoiceSingle,ot as default};
---
+++ snack-uikit/assets/NotificationCard.story-gBgiUZKX.js
@@ -0,0 +1,44 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{c as C}from"./index-DUmpqB7p.js";import{r as N}from"./index-CJ_LmFaV.js";import{N as n,c as p,a as h,b as A,S as j,d as i,A as u,h as b,s as o}from"./styles.module-DEnhkXCO.js";import"./index-ChsGqxH_.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-CAfvFIBF.js";import"./index-B80hK4m0.js";import"./DropList-BeTwO9JS.js";import"./index-B7wZyJkK.js";import"./hooks-DON7lT9t.js";import"./componentPropsProcessors-UJEZy8aQ.js";import"./useValueControl-CNLw7ht-.js";import"./index-JZGyZbA2.js";import"./Checkbox-B-86ZbLV.js";import"./constants-BObEONKm.js";import"./TogglePrivate-BTSew7b5.js";import"./index-w0WAZL2A.js";import"./index-Y3TRpJ-L.js";import"./TruncateString-BMtbUp3O.js";import"./useIsomorphicLayoutEffect-BAXQmlEq.js";import"./isBrowser-C2N6XvuH.js";import"./Tooltip-DzAF_V2U.js";import"./PopoverPrivate-CnKgsFAm.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./Switch-Cq7kjPHZ.js";import"./index-DHAmGKAZ.js";import"./index-DreyRvUE.js";import"./Dropdown-4YXb2YvP.js";import"./index-C3mvkShn.js";import"./index-CXJdfWGn.js";import"./LocaleProvider-B4VrUkyw.js";import"./index-BhVwnnbR.js";import"./IconPredefined-DVxQYij_.js";import"./Typography-COVcnQ5F.js";import"./ButtonTonal-BOArSFPu.js";import"./utils-CWv1hTMo.js";import"./Counter-DXWSEFY6.js";import"./Sun-CjKF0Bsh.js";import"./constants-BAH7I9kc.js";import"./ButtonFilled-pGiP3SQa.js";import"./Divider-C1X9zrkx.js";import"./Scroll-CyeY1SIb.js";import"./Spinner-PYn9r2mo.js";import"./ButtonFunction-DO8-q7Gq.js";import"./useButtonNavigation-CVCsehRW.js";import"./useClearButton-CAlOraVE.js";import"./constants-9uwKbtvo.js";import"./InputPrivate-alc9Nisi.js";import"./Tag-Dyz5wRNP.js";import"./constants-BaREPXw5.js";import"./ButtonSimple-BlHO0UW0.js";import"./Link-D1V5U7hr.js";import"./helpers-MlN6OFO3.js";import"./index-Chjiymov.js";import"./ProgressBar-TiBN-Oau.js";import"./index-Dn6vuaYJ.js";import"./index-DbF-M2Rm.js";const wt={title:"Components/Notification",component:n},x=["Default","Unread"];function T(e){return t.jsxs(t.Fragment,{children:[t.jsx("div",{className:o.cell,children:t.jsx(n,{...i,className:o.card,...e})}),t.jsx("div",{className:o.cell,children:t.jsx(n,{...i,className:o.card,unread:!0,...e})})]})}const s=C(o.cell,o.headerCell),S=({showButtons:e,...g})=>t.jsxs("div",{className:o.pageWrapper,children:[t.jsxs("div",{className:o.controlledWrapper,children:["Controlled:",t.jsx(n,{...g,...e?{primaryButton:{label:"Primary Button"},secondaryButton:{label:"Secondary Button"}}:{},className:o.controlledCard})]}),t.jsxs("div",{className:o.table,children:[t.jsx("div",{className:s,style:{gridRow:"1 / 1"}}),x.map((r,m)=>t.jsx("div",{className:s,style:{gridColumnStart:m+2,gridColumnEnd:m+3},children:r},r)),Object.values(u).map(r=>t.jsxs(N.Fragment,{children:[t.jsx("div",{className:s,children:r}),t.jsx(T,{appearance:r})]},r))]})]}),a={render:S,args:{...i,actions:i.actions.map(e=>({...e,onClick:b})),"data-test-id":j.card,showButtons:!0},argTypes:{appearance:{options:Object.values(u),control:{type:"radio"}},showButtons:{name:"[Stories]: Show action buttons"},primaryButton:{table:{disable:!0}},secondaryButton:{table:{disable:!0}}},parameters:{readme:{sidebar:[`Latest version: ${p.version}`,h,A]},packageName:p.name,design:{name:"Figma",type:"figma",url:"https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A209668&mode=design"}}};var c,l,d;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    ...NOTIFICATION_CARD_MOCK,
+    actions: NOTIFICATION_CARD_MOCK.actions.map(action => ({
+      ...action,
+      onClick: handleActionClick
+    })),
+    'data-test-id': STORY_TEST_IDS.card,
+    showButtons: true
+  },
+  argTypes: {
+    appearance: {
+      options: Object.values(APPEARANCE),
+      control: {
+        type: 'radio'
+      }
+    },
+    showButtons: {
+      name: '[Stories]: Show action buttons'
+    },
+    primaryButton: {
+      table: {
+        disable: true
+      }
+    },
+    secondaryButton: {
+      table: {
+        disable: true
+      }
+    }
+  },
+  parameters: {
+    readme: {
+      sidebar: [\`Latest version: \${componentPackage.version}\`, componentReadme, componentChangelog]
+    },
+    packageName: componentPackage.name,
+    design: {
+      name: 'Figma',
+      type: 'figma',
+      url: 'https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A209668&mode=design'
+    }
+  }
+}`,...(d=(l=a.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};export{wt as default,a as notificationCard};
