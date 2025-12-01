27ffc93f8abe4f2eb564cde30ee5d1a7f2dbb212
cloud-ru-tech-robot <cloud-ru-tech-robot>
2025-12-01 10:23:37 +0000

    new storybook version

---
+++ snack-uikit/assets/IconPredefined.story-hy6YSrG7.js
@@ -0,0 +1,243 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{I as o}from"./index-JswG3_7a.js";import{I as d,A as r,S as m}from"./IconPredefined-DVxQYij_.js";import"./index-ChsGqxH_.js";import"./Sprite-DoNyyhE0.js";import"./index-CJ_LmFaV.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./sprite.symbol-CrFdCDis.js";import"./index-CAfvFIBF.js";import"./index-BAKcRSlX.js";import"./index-CKE7Rdrn.js";import"./index-Y3TRpJ-L.js";import"./index-DreyRvUE.js";import"./index-Ca89sW9R.js";import"./index-C3mvkShn.js";import"./index-BGTKE3tV.js";import"./index-DHAmGKAZ.js";import"./index-h0kOFyIh.js";import"./index-BfUMklLA.js";import"./index-B0xwoRpp.js";import"./index-B2h5DBoi.js";import"./index-5mF7KA5g.js";import"./index-WpT-B0mJ.js";import"./index-B_69vUNX.js";import"./index-B80hK4m0.js";import"./index-w0WAZL2A.js";import"./index-Dn6vuaYJ.js";import"./index-CTTgWa-s.js";import"./index-CF_OgQNJ.js";import"./index-DBTBMFOQ.js";import"./index-CXJdfWGn.js";import"./index-x12pshUM.js";import"./index-CGSydWJd.js";import"./index-DbF-M2Rm.js";import"./index-DUmpqB7p.js";import"./componentPropsProcessors-UJEZy8aQ.js";const p=`# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+## 0.7.10 (2025-09-01)
+
+### Only dependencies have been changed
+* [@snack-uikit/utils@4.0.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+
+
+
+
+## 0.7.9 (2025-07-16)
+
+### Only dependencies have been changed
+* [@snack-uikit/utils@3.10.1](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+
+
+
+
+## 0.7.8 (2025-07-15)
+
+### Only dependencies have been changed
+* [@snack-uikit/utils@3.10.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+
+
+
+
+## 0.7.7 (2025-07-07)
+
+### Only dependencies have been changed
+* [@snack-uikit/utils@3.9.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+
+
+
+
+## 0.7.6 (2025-05-16)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.8.2](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.7.5 (2025-03-17)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.8.1](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.7.4 (2025-03-04)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.8.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.7.3 (2025-01-23)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.7.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.7.2 (2024-12-12)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.6.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.7.1 (2024-11-14)
+
+### Dependencies
+
+- **FF-5678:** up deps classnames/uncontrollable ([8f0d645](https://github.com/cloud-ru-tech/snack-uikit/commit/8f0d645fc7eb8eaf95660cd0ae7d4b550821059b))
+
+# 0.7.0 (2024-10-28)
+
+### Features
+
+- **FF-5782:** sass use/forward approach ([3e53869](https://github.com/cloud-ru-tech/snack-uikit/commit/3e53869ace864a7718e434b7f410c15dbd911cd5))
+
+# 0.6.0 (2024-10-28)
+
+### Features
+
+- **FF-5502:** add cjs build ([ffada28](https://github.com/cloud-ru-tech/snack-uikit/commit/ffada28bfdc37ea760eb1c8759342e680bdf8dd6))
+
+## 0.5.2 (2024-09-23)
+
+### Bug Fixes
+
+- **FF-3788:** updated typescript to v5 ([89e3338](https://github.com/cloud-ru-tech/snack-uikit/commit/89e3338a3ef4df24e4c20d9a9c8cd16e7934ba63))
+
+## 0.5.1 (2024-04-23)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.3.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+# 0.5.0 (2024-02-28)
+
+### Features
+
+- **FF-4147:** add shape prop into IconPredefined ([ed019ff](https://github.com/cloud-ru-tech/snack-uikit/commit/ed019fff271dd04196a7bb5f824c65256de0d714))
+
+## 0.4.2 (2024-02-20)
+
+### Bug Fixes
+
+- **FF-4297:** apply system-layer tokens ([7c8fd4b](https://github.com/cloud-ru-tech/snack-uikit/commit/7c8fd4b5334360b2fc31da92973b6835ffa287af))
+
+## 0.4.1 (2023-12-28)
+
+### Bug Fixes
+
+- **FF-3960:** force current color on svg ([e87bc53](https://github.com/cloud-ru-tech/snack-uikit/commit/e87bc5312c36f36b001cf47f3ca74027fc4624a5))
+
+# 0.4.0 (2023-12-14)
+
+### BREAKING CHANGES
+
+- **FF-3729:** replace enum with unions ([910db4a](https://github.com/cloud-ru-tech/snack-uikit/commit/910db4aa8231ccbc58e538e5c5c1f461b1dec275))
+
+## 0.3.1 (2023-12-06)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.1.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+# 0.3.0 (2023-12-06)
+
+### Bug Fixes
+
+- **FF-0000:** change icon to size-s ([6c36bad](https://github.com/cloud-ru-tech/snack-uikit/commit/6c36bad3dcbb04d3639c91db2f5c203e5655bfd3))
+- **FF-0000:** remove extractDataProps ([9d676b9](https://github.com/cloud-ru-tech/snack-uikit/commit/9d676b9df214167bfd1b7ce9f1590891ca62f9a3))
+- **FF-2777:** using new icons ([7922fca](https://github.com/cloud-ru-tech/snack-uikit/commit/7922fca103293299554fe07d607ca54b3b571e66))
+- **FF-3022:** show proper icon depending on size ([5f2d71f](https://github.com/cloud-ru-tech/snack-uikit/commit/5f2d71fea4e0cdcf8ce1a0e7a816676b668450e8))
+- **FF-3130:** flex-shrink ([7dbcf62](https://github.com/cloud-ru-tech/snack-uikit/commit/7dbcf628612659cb2f1c77aa6f0c6b4f783e13ea))
+
+### Features
+
+- **FF-2267:** add icon component ([cf25f4c](https://github.com/cloud-ru-tech/snack-uikit/commit/cf25f4c709bf7f8abfad80f3da1ebd6f6f14883d))
+- **FF-875:** add utils for dependencies graph ([53cd7be](https://github.com/cloud-ru-tech/snack-uikit/commit/53cd7be638f01e573cb52b2417a39f4df4f6089b))
+
+### BREAKING CHANGES
+
+- **FF-3823:** move to snack-uikit scope ([ac9e30d](https://github.com/cloud-ru-tech/snack-uikit/commit/ac9e30d574d529b2bf4f5184b70c511c981a6032))
+
+## 0.2.5 (2023-11-08)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@2.0.1](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.2.4 (2023-10-31)
+
+### Bug Fixes
+
+- **FF-0000:** change icon to size-s ([6c36bad](https://github.com/cloud-ru-tech/snack-uikit/commit/6c36bad3dcbb04d3639c91db2f5c203e5655bfd3))
+- **FF-0000:** remove extractDataProps ([9d676b9](https://github.com/cloud-ru-tech/snack-uikit/commit/9d676b9df214167bfd1b7ce9f1590891ca62f9a3))
+
+## 0.2.3 (2023-10-25)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@1.3.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.2.2 (2023-10-13)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@2.0.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.2.1 (2023-10-06)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.0.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+# 0.2.0 (2023-09-14)
+
+### Bug Fixes
+
+- **FF-3130:** flex-shrink ([7dbcf62](https://github.com/cloud-ru-tech/snack-uikit/commit/7dbcf628612659cb2f1c77aa6f0c6b4f783e13ea))
+
+### Features
+
+- **FF-875:** add utils for dependencies graph ([53cd7be](https://github.com/cloud-ru-tech/snack-uikit/commit/53cd7be638f01e573cb52b2417a39f4df4f6089b))
+
+## 0.1.2 (2023-08-28)
+
+### Bug Fixes
+
+- **FF-2777:** using new icons ([7922fca](https://github.com/cloud-ru-tech/snack-uikit/commit/7922fca103293299554fe07d607ca54b3b571e66))
+- **FF-3022:** show proper icon depending on size ([5f2d71f](https://github.com/cloud-ru-tech/snack-uikit/commit/5f2d71fea4e0cdcf8ce1a0e7a816676b668450e8))
+
+## 0.1.1 (2023-07-28)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.2.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+# 0.1.0 (2023-06-28)
+
+### Features
+
+- **FF-2267:** add icon component ([cf25f4c](https://github.com/cloud-ru-tech/snack-uikit/commit/cf25f4c709bf7f8abfad80f3da1ebd6f6f14883d))
+
+## CHANGELOG
+
+### v0.0.0
+
+- Initial version
+`,l="@snack-uikit/icon-predefined",b="0.7.10",c={name:l,version:b},h='# Icon Predefined\n\n## Installation\n`npm i @snack-uikit/icon-predefined`\n\n[Changelog](./CHANGELOG.md)\n\n## Example\n\n```tsx\nimport { HeartSVG } from \'@snack-uikit/icons\';\nimport { IconPredefined } from \'@snack-uikit/icon-predefined\';\n\n<IconPredefined\n    appearance=\'red\'\n    size=\'l\'\n    className=\'className\'\n    icon={HeartSVG}\n    decor\n/>\n```\n\n## Props\n\n#### **`icon: JSXElementConstructor`**\nКомпонент svg-иконки из пакета `@snack-uikit/icons`.\n\n#### **`className?: string`**\ncss-класс контейнера.\n\n#### **`appearance?: Appearance`** - *`primary`*\nЦвет иконки. Возможные варианты:\n  - `primary`\n  - `neutral`\n  - `red`\n  - `orange`\n  - `yellow`\n  - `green`\n  - `blue`\n  - `violet`\n  - `pink`\n\n#### **`size?: Size`** - *`s`*\nРазмер компонента. Возможные варианты:\n  - `s`\n  - `m`\n  - `l`\n\n#### **`decor?: boolean`** - *`[true]`*\nВключает цветную подложку для иконки.\n\n[//]: DOCUMENTATION_SECTION_START\n[//]: THIS_SECTION_IS_AUTOGENERATED_PLEASE_DONT_EDIT_IT\n## IconPredefined\n### Props\n| name | type | default value | description |\n|------|------|---------------|-------------|\n| icon* | `JSXElementConstructor<{ size?: number; className?: string; }>` | - | JSX иконки |\n| className | `string` | - | CSS-класс |\n| appearance | enum Appearance: `"primary"`, `"neutral"`, `"red"`, `"orange"`, `"yellow"`, `"green"`, `"blue"`, `"violet"`, `"pink"` | primary | Внешний вид |\n| decor | `boolean` | true | Наличие цветной подложки |\n| size | enum Size: `"s"`, `"m"`, `"l"` | m | Размер |\n| shape | "round" \\| "square" | round | Форма: круглая или квадратная |\n\n\n[//]: DOCUMENTATION_SECTION_END\n',k="_wrapper_1q8b9_1",f={wrapper:k},tn={title:"Components/Icon Predefined",component:d},g=({icon:e,...u})=>t.jsx("div",{className:f.wrapper,children:t.jsx(d,{...u,icon:o[e]})}),F=Object.keys(o).filter(e=>e!=="Sprite"),n={render:g,args:{size:m.M,appearance:r.Primary,decor:!0,icon:"PlaceholderSVG"},argTypes:{icon:{options:F,control:{type:"select"}}},parameters:{readme:{sidebar:[`Latest version: ${c.version}`,h,p]},packageName:c.name,design:{name:"Figma",type:"figma",url:"https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A20598&mode=design"}}};var i,a,s;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    size: SIZE.M,
+    appearance: APPEARANCE.Primary,
+    decor: true,
+    icon: 'PlaceholderSVG'
+  },
+  argTypes: {
+    icon: {
+      options: iconNames,
+      control: {
+        type: 'select'
+      }
+    }
+  },
+  parameters: {
+    readme: {
+      sidebar: [\`Latest version: \${componentPackage.version}\`, componentReadme, componentChangelog]
+    },
+    packageName: componentPackage.name,
+    design: {
+      name: 'Figma',
+      type: 'figma',
+      url: 'https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A20598&mode=design'
+    }
+  }
+}`,...(s=(a=n.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};export{n as IconPredefined,tn as default};
