4194625b0d388bf687d3ac3496fe5b9a70f4bc20
cloud-ru-tech-robot <cloud-ru-tech-robot>
2025-12-19 14:52:43 +0000

    new storybook version

---
+++ snack-uikit/assets/FieldDecorator.story-Cyx3GuiM.js
@@ -0,0 +1,29 @@
+import{j as t}from"./jsx-runtime-BTJTZTIL.js";import{c as i,a as l,b as d,C as a,s as r}from"./styles.module-C2UZosxg.js";import{F as s}from"./FieldDecorator-Clwd4F5J.js";import{T as c}from"./Typography-DLfEOGp8.js";import"./index-ChsGqxH_.js";import"./index-JswG3_7a.js";import"./Sprite-DoNyyhE0.js";import"./index-CJ_LmFaV.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./sprite.symbol-CrFdCDis.js";import"./index-CAfvFIBF.js";import"./index-BAKcRSlX.js";import"./index-CKE7Rdrn.js";import"./index-Y3TRpJ-L.js";import"./index-DreyRvUE.js";import"./index-Ca89sW9R.js";import"./index-C3mvkShn.js";import"./index-BGTKE3tV.js";import"./index-DHAmGKAZ.js";import"./index-h0kOFyIh.js";import"./index-BfUMklLA.js";import"./index-B0xwoRpp.js";import"./index-B2h5DBoi.js";import"./index-5mF7KA5g.js";import"./index-WpT-B0mJ.js";import"./index-B_69vUNX.js";import"./index-B80hK4m0.js";import"./index-w0WAZL2A.js";import"./index-Dn6vuaYJ.js";import"./index-CTTgWa-s.js";import"./index-CF_OgQNJ.js";import"./index-DBTBMFOQ.js";import"./index-CXJdfWGn.js";import"./index-x12pshUM.js";import"./index-CGSydWJd.js";import"./index-DbF-M2Rm.js";import"./index-DUmpqB7p.js";import"./constants-9uwKbtvo.js";import"./TruncateString-D54BiV_x.js";import"./useIsomorphicLayoutEffect-BAXQmlEq.js";import"./isBrowser-C2N6XvuH.js";import"./componentPropsProcessors-UJEZy8aQ.js";import"./Tooltip-DzAF_V2U.js";import"./PopoverPrivate-CnKgsFAm.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./index-JZGyZbA2.js";const pt={title:"Components/Fields/Field Decorator",component:s},g=o=>t.jsx("div",{className:r.wrapper,"data-size":o.size,children:t.jsx(s,{...o,children:t.jsx("div",{className:r.decoratorStory,children:t.jsx(c.SansBodyM,{children:"Custom content - ReactNode"})})})}),e={render:g,args:{label:"Label text",labelTooltip:"Tooltip description",required:!1,caption:"Caption",hint:"Hint text",size:"s",readonly:!1,validationState:"default",disabled:!1},argTypes:{validationState:a.validationState,labelTooltip:a.labelTooltip},parameters:{readme:{sidebar:[`Latest version: ${i.version}`,l,d]},packageName:i.name,design:{name:"Figma",type:"figma",url:"https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-2.0.0?type=design&node-id=41%3A38747&mode=design&t=8dDi5X6Lfgs6Cxji-1"}}};var n,p,m;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    label: 'Label text',
+    labelTooltip: 'Tooltip description',
+    required: false,
+    caption: 'Caption',
+    hint: 'Hint text',
+    size: 's',
+    readonly: false,
+    validationState: 'default',
+    disabled: false
+  },
+  argTypes: {
+    validationState: COMMON_ARG_TYPES.validationState,
+    labelTooltip: COMMON_ARG_TYPES.labelTooltip
+  },
+  parameters: {
+    readme: {
+      sidebar: [\`Latest version: \${componentPackage.version}\`, componentReadme, componentChangelog]
+    },
+    packageName: componentPackage.name,
+    design: {
+      name: 'Figma',
+      type: 'figma',
+      url: 'https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-2.0.0?type=design&node-id=41%3A38747&mode=design&t=8dDi5X6Lfgs6Cxji-1'
+    }
+  }
+}`,...(m=(p=e.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};export{pt as default,e as fieldDecorator};
---
+++ snack-uikit/assets/Skeleton.story-DIL-elrX.js
@@ -0,0 +1,432 @@
+import{j as e}from"./jsx-runtime-BTJTZTIL.js";import{a as r,S as N}from"./Skeleton-Cf0sBfNU.js";import{T as O,P as T,S as G}from"./Typography-DLfEOGp8.js";import{T as m}from"./TruncateString-D54BiV_x.js";import{S as C}from"./SkeletonText-DP-re6Ou.js";import{W as E}from"./WithSkeleton-BvFnKWIX.js";import"./index-ChsGqxH_.js";import"./index-DUmpqB7p.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-CJ_LmFaV.js";import"./componentPropsProcessors-UJEZy8aQ.js";import"./useIsomorphicLayoutEffect-BAXQmlEq.js";import"./isBrowser-C2N6XvuH.js";import"./Tooltip-DzAF_V2U.js";import"./PopoverPrivate-CnKgsFAm.js";import"./index-Bxu_udwM.js";import"./index-DdXvKv-Q.js";import"./index-JZGyZbA2.js";const A=`# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+## 0.6.9 (2025-09-01)
+
+### Only dependencies have been changed
+* [@snack-uikit/utils@4.0.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+
+
+
+
+## 0.6.8 (2025-07-16)
+
+### Only dependencies have been changed
+* [@snack-uikit/utils@3.10.1](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+
+
+
+
+## 0.6.7 (2025-07-15)
+
+### Only dependencies have been changed
+* [@snack-uikit/utils@3.10.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+
+
+
+
+## 0.6.6 (2025-07-07)
+
+### Only dependencies have been changed
+* [@snack-uikit/utils@3.9.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+
+
+
+
+## 0.6.5 (2025-05-16)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.8.2](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.6.4 (2025-03-17)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.8.1](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.6.3 (2025-03-04)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.8.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.6.2 (2025-01-23)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.7.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.6.1 (2024-12-12)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.6.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+# 0.6.0 (2024-11-29)
+
+### Features
+
+- **PDS-801:** add typography prop ([b0e8495](https://github.com/cloud-ru-tech/snack-uikit/commit/b0e84950550b866c3a4460b4b31143df498d705b))
+
+### BREAKING CHANGES
+
+- **PDS-801:** use skeleton text tokens ([930bcb0](https://github.com/cloud-ru-tech/snack-uikit/commit/930bcb01500e5754457a8d1e8fdf2c0487bc2526))
+
+## 0.5.1 (2024-11-14)
+
+### Dependencies
+
+- **FF-5678:** up deps classnames/uncontrollable ([8f0d645](https://github.com/cloud-ru-tech/snack-uikit/commit/8f0d645fc7eb8eaf95660cd0ae7d4b550821059b))
+
+# 0.5.0 (2024-10-28)
+
+### Features
+
+- **FF-5782:** sass use/forward approach ([3e53869](https://github.com/cloud-ru-tech/snack-uikit/commit/3e53869ace864a7718e434b7f410c15dbd911cd5))
+
+# 0.4.0 (2024-10-28)
+
+### Features
+
+- **FF-5502:** add cjs build ([ffada28](https://github.com/cloud-ru-tech/snack-uikit/commit/ffada28bfdc37ea760eb1c8759342e680bdf8dd6))
+
+## 0.3.5 (2024-09-23)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.4.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.3.4 (2024-04-23)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.3.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.3.3 (2024-02-20)
+
+### Bug Fixes
+
+- **FF-4297:** apply system-layer tokens ([7c8fd4b](https://github.com/cloud-ru-tech/snack-uikit/commit/7c8fd4b5334360b2fc31da92973b6835ffa287af))
+
+## 0.3.2 (2023-12-14)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.2.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.3.1 (2023-12-06)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.1.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+# 0.3.0 (2023-12-06)
+
+### Bug Fixes
+
+- **FF-0:** move utils to usual deps ([ebe441a](https://github.com/cloud-ru-tech/snack-uikit/commit/ebe441ac398065cbe8523cbedd3df53176b9aea5))
+- **FF-2257:** up utils ([5dd5d28](https://github.com/cloud-ru-tech/snack-uikit/commit/5dd5d28cdbe14973dcc36759e7db003249930a4b))
+
+### Features
+
+- **FF-2205:** add skeleton package ([fa3467c](https://github.com/cloud-ru-tech/snack-uikit/commit/fa3467ccf95cb54463575726241ba8631b936d35))
+- **FF-875:** add utils for dependencies graph ([53cd7be](https://github.com/cloud-ru-tech/snack-uikit/commit/53cd7be638f01e573cb52b2417a39f4df4f6089b))
+
+### BREAKING CHANGES
+
+- **FF-3823:** move to snack-uikit scope ([ac9e30d](https://github.com/cloud-ru-tech/snack-uikit/commit/ac9e30d574d529b2bf4f5184b70c511c981a6032))
+
+## 0.2.4 (2023-11-08)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@2.0.1](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.2.3 (2023-10-31)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@2.0.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.2.2 (2023-10-25)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@1.3.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.2.1 (2023-10-13)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.0.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+# 0.2.0 (2023-10-06)
+
+### Features
+
+- **FF-875:** add utils for dependencies graph ([53cd7be](https://github.com/cloud-ru-tech/snack-uikit/commit/53cd7be638f01e573cb52b2417a39f4df4f6089b))
+
+## 0.1.5 (2023-07-28)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@3.5.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.1.4 (2023-06-23)
+
+**Note:** Version bump only for package @snack-uikit/skeleton
+
+## 0.1.3 (2023-06-20)
+
+### Only dependencies have been changed
+
+- [@snack-uikit/utils@1.2.0](https://github.com/cloud-ru-tech/snack-uikit/blob/master/packages/utils/CHANGELOG.md)
+
+## 0.1.2 (2023-06-14)
+
+### Bug Fixes
+
+- **FF-0:** move utils to usual deps ([ebe441a](https://github.com/cloud-ru-tech/snack-uikit/commit/ebe441ac398065cbe8523cbedd3df53176b9aea5))
+
+## 0.1.1 (2023-05-26)
+
+### Bug Fixes
+
+- **FF-2257:** up utils ([5dd5d28](https://github.com/cloud-ru-tech/snack-uikit/commit/5dd5d28cdbe14973dcc36759e7db003249930a4b))
+
+# 0.1.0 (2023-05-16)
+
+### Features
+
+- **FF-2205:** add skeleton package ([fa3467c](https://github.com/cloud-ru-tech/snack-uikit/commit/fa3467ccf95cb54463575726241ba8631b936d35))
+
+## CHANGELOG
+
+### v0.0.0
+
+- Initial version
+`,_="@snack-uikit/skeleton",L="0.6.9",f={name:_,version:L},F=`# Skeleton
+
+## Installation
+\`npm i @snack-uikit/skeleton\`
+
+[Changelog](./CHANGELOG.md)
+
+## Description
+
+- Пакет \`@snack-uikit/skeleton\` предоставляет компоненты для отображения индикаторов загрузки (скелетонов) во время асинхронных операций.
+- \`Skeleton\` — базовый компонент для создания блочных скелетонов произвольной формы с настраиваемыми размерами (ширина, высота) и радиусом скругления.
+- \`SkeletonText\` — специализированный компонент для имитации текстового контента: отображает несколько строк скелетона с учётом типографики (размер шрифта, межстрочный интервал), что позволяет точно соответствовать реальному тексту по высоте и визуальному стилю.
+- \`WithSkeleton\` — компонент-обёртка для группировки блоков скелетона: принимает JSX-ноду скелетона в проп \`skeleton\` и контент в \`children\`, автоматически переключаясь между ними в зависимости от состояния загрузки.
+- \`SkeletonContextProvider\` — контекст-провайдер для централизованного управления состоянием загрузки: находящиеся внутри этого контекста компоненты \`Skeleton\` и \`SkeletonText\` автоматически получают значение \`loading\` из контекста и не требуют явной передачи этого пропа.
+- Все компоненты поддерживают работу как с явным пропом \`loading\`, так и с контекстом: если проп не передан, значение берётся из \`SkeletonContextProvider\`.
+
+## Example
+
+\`\`\`tsx
+import { Skeleton, SkeletonText, SkeletonContextProvider, WithSkeleton } from '@snack-uikit/skeleton';
+
+function Example() {
+  const isLoading = true;
+
+  return (
+    <>
+      {/* Базовое использование Skeleton */}
+      <Skeleton loading={isLoading} width={200} height={100} borderRadius={8}>
+        <div>Контент после загрузки</div>
+      </Skeleton>
+
+      {/* Использование SkeletonText с типографикой */}
+      <SkeletonText loading={isLoading} lines={3} typography="body-m" width="100%">
+        <p>Текст, который появится после загрузки</p>
+      </SkeletonText>
+
+      {/* Централизованное управление через контекст */}
+      <SkeletonContextProvider loading={isLoading}>
+        <Skeleton width={100} height={100} borderRadius={50}>
+          <Avatar />
+        </Skeleton>
+        <SkeletonText lines={2}>
+          <Title />
+        </SkeletonText>
+      </SkeletonContextProvider>
+
+      {/* Группировка блоков с WithSkeleton */}
+      <SkeletonContextProvider loading={isLoading}>
+        <WithSkeleton
+          skeleton={
+            <div>
+              <Skeleton width={100} height={100} borderRadius={50} />
+              <SkeletonText lines={3} />
+            </div>
+          }
+        >
+          <Card />
+        </WithSkeleton>
+      </SkeletonContextProvider>
+    </>
+  );
+}
+\`\`\`
+
+[//]: DOCUMENTATION_SECTION_START
+[//]: THIS_SECTION_IS_AUTOGENERATED_PLEASE_DONT_EDIT_IT
+## Skeleton
+### Props
+| name | type | default value | description |
+|------|------|---------------|-------------|
+| loading | \`boolean\` | - | Флаг состояния загрузки. Если значение true, будет отрисован блок скелетона, если false - children. |
+| width | \`Width<string \\| number>\` | - | Ширина блока. Можно указать значение допустимое для CSSProperty.width (пример \`'60%'\`, \`'400px'\` и т.д) |
+| height | \`Height<string \\| number>\` | - | Высота блока. Можно указать значение допустимое для CSSProperty.height (пример \`'60%'\`, \`'400px'\` и т.д) |
+| borderRadius | \`BorderRadius<string \\| number>\` | - | Радиус скругления. Можно указать значение допустимое для CSSProperty.borderRadius (пример \`'10px'\`, \`'50%'\` и т.д) |
+| className | \`string\` | - | CSS-класс |
+## SkeletonText
+### Props
+| name | type | default value | description |
+|------|------|---------------|-------------|
+| loading | \`boolean\` | - | Флаг состояния загрузки. Если значение true, будет отрисован блок скелетона, если false - children. |
+| width | \`Width<string \\| number>\` | - | Ширина блока. Можно указать значение допустимое для CSSProperty.width (пример \`'60%'\`, \`'400px'\` и т.д) |
+| className | \`string\` | - | CSS-класс |
+| lines | \`number\` | 3 | Количество строк. |
+| rowClassName | \`string\` | - | CSS-класс строки |
+| lineClassName | \`string\` | - | CSS-класс линии |
+| typography | "display-l" \\| "display-m" \\| "display-s" \\| "headline-l" \\| "headline-m" \\| "headline-s" \\| "title-l" \\| "title-m" \\| "title-s" \\| "label-l" \\| "label-m" \\| "label-s" \\| "body-l" \\| "body-m" \\| "body-s" | body-m | Типографика |
+## WithSkeleton
+### Props
+| name | type | default value | description |
+|------|------|---------------|-------------|
+| skeleton* | \`ReactNode\` | - | JSX скелетон |
+| loading | \`boolean\` | - | Флаг состояния загрузки. Если значение true, будет отрисован блок скелетона, если false - children. |
+## SkeletonContextProvider
+### Props
+| name | type | default value | description |
+|------|------|---------------|-------------|
+| loading* | \`boolean\` | - | Флаг состояния загрузки. Если значение true, будут отрисованы блоки скелетона. |
+
+
+[//]: DOCUMENTATION_SECTION_END
+`,j="_textContainer_8mj9o_1",P="_storyCard_8mj9o_12",H="_storyCard_header_8mj9o_19",R="_storyCard_avatar_8mj9o_26",w="_storyCard_title_8mj9o_36",i={textContainer:j,storyCard:P,storyCard_header:H,storyCard_avatar:R,storyCard_title:w},ae={title:"Components/Skeleton",component:r},l={readme:{sidebar:[`Latest version: ${f.version}`,F,A]}},u=f.name;function I({...n}){return e.jsx(r,{...n,children:e.jsx("div",{"data-test-id":"children"})})}const a={render:I,args:{loading:!0},argTypes:{width:{type:"string"},height:{type:"string"},borderRadius:{type:"string"}},parameters:{...l,packageName:u,controls:{exclude:["className"]}}},D=Object.values(T).reduce((n,t)=>(Object.values(G).forEach(s=>{n.push([t,s].join("-"))}),n),[]),o={render:({text:n,typography:t,textOpacity:s,...d})=>e.jsx(O,{family:"sans",size:(t==null?void 0:t.split("-")[1])||"m",purpose:(t==null?void 0:t.split("-")[0])||"body",children:e.jsxs("div",{className:i.textContainer,style:{position:"relative"},children:[e.jsx("div",{style:{opacity:s/100},children:e.jsx(m,{text:n,maxLines:d.lines})}),e.jsx("div",{style:{position:"absolute",left:0,top:0},children:e.jsx(C,{...d,typography:t,children:e.jsx("div",{"data-test-id":"children",children:e.jsx(m,{text:n,maxLines:d.lines})})})})]})}),args:{text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nesciunt consequuntur veniam libero aliquid perspiciatis earum quasi natus unde saepe provident, aliquam maiores dolor. Illum possimus modi saepe architecto voluptatibus!",textOpacity:50,typography:"body-m",lines:3,loading:!0},parameters:{...l,packageName:u,controls:{exclude:["width","height","className"]}},argTypes:{lines:{type:"number"},text:{name:"[Stories]: demo text",type:"string"},typography:{options:D,control:{type:"select"}},textOpacity:{name:"[Stories]: demo text opacity",control:{type:"range",min:0,max:100,step:1}}}};function W({children:n}){return e.jsx("div",{"data-test-id":"children",className:i.storyCard,children:e.jsxs("div",{className:i.storyCard_header,children:[e.jsx("div",{className:i.storyCard_avatar}),e.jsx("div",{className:i.storyCard_title,children:n})]})})}const c={render:({...n})=>{const t=e.jsx("div",{"data-test-id":n["data-test-id"],className:i.storyCard,children:e.jsxs("div",{className:i.storyCard_header,children:[e.jsx(r,{width:100,height:100,borderRadius:50}),e.jsx("div",{className:i.storyCard_title,children:e.jsx(C,{lines:3})})]})});return e.jsx(N,{loading:n.loading,children:["Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, laboriosam iure!","Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi cupiditate qui amet.","Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, provident."].map(s=>e.jsx(E,{skeleton:t,children:e.jsx(W,{children:s})},s))})},args:{loading:!0},parameters:{...l,packageName:u,controls:{exclude:["width","height","borderRadius","className"]}}};var p,h,k;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    loading: true
+  },
+  argTypes: {
+    width: {
+      type: 'string'
+    },
+    height: {
+      type: 'string'
+    },
+    borderRadius: {
+      type: 'string'
+    }
+  },
+  parameters: {
+    ...readmeAndDesign,
+    packageName,
+    controls: {
+      exclude: ['className']
+    }
+  }
+}`,...(k=(h=a.parameters)==null?void 0:h.docs)==null?void 0:k.source}}};var b,g,y;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  render: ({
+    text,
+    typography,
+    textOpacity,
+    ...args
+  }: skeletonTextStoryArgs) => <Typography family='sans' size={typography?.split('-')[1] as Size || 'm'} purpose={typography?.split('-')[0] as Purpose || 'body'}>
+      <div className={styles.textContainer} style={{
+      position: 'relative'
+    }}>
+        <div style={{
+        opacity: textOpacity / 100
+      }}>
+          <TruncateString text={text} maxLines={args.lines} />
+        </div>
+        <div style={{
+        position: 'absolute',
+        left: 0,
+        top: 0
+      }}>
+          <SkeletonText {...args} typography={typography}>
+            <div data-test-id='children'>
+              <TruncateString text={text} maxLines={args.lines} />
+            </div>
+          </SkeletonText>
+        </div>
+      </div>
+    </Typography>,
+  args: {
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nesciunt consequuntur veniam libero aliquid perspiciatis earum quasi natus unde saepe provident, aliquam maiores dolor. Illum possimus modi saepe architecto voluptatibus!',
+    textOpacity: 50,
+    typography: 'body-m',
+    lines: 3,
+    loading: true
+  },
+  parameters: {
+    ...readmeAndDesign,
+    packageName,
+    controls: {
+      exclude: ['width', 'height', 'className']
+    }
+  },
+  argTypes: {
+    lines: {
+      type: 'number'
+    },
+    text: {
+      name: '[Stories]: demo text',
+      type: 'string'
+    },
+    typography: {
+      options: VARIANT,
+      control: {
+        type: 'select'
+      }
+    },
+    textOpacity: {
+      name: '[Stories]: demo text opacity',
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+        step: 1
+      }
+    }
+  }
+}`,...(y=(g=o.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var x,S,v;c.parameters={...c.parameters,docs:{...(x=c.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  render: ({
+    ...args
+  }) => {
+    const cardSkeleton = <div data-test-id={args['data-test-id']} className={styles.storyCard}>
+        <div className={styles.storyCard_header}>
+          <Skeleton width={100} height={100} borderRadius={50} />
+          <div className={styles.storyCard_title}>
+            <SkeletonText lines={3} />
+          </div>
+        </div>
+      </div>;
+    return <SkeletonContextProvider loading={args.loading}>
+        {['Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, laboriosam iure!', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi cupiditate qui amet.', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, provident.'].map(title => <WithSkeleton key={title} skeleton={cardSkeleton}>
+            <Card>{title}</Card>
+          </WithSkeleton>)}
+      </SkeletonContextProvider>;
+  },
+  args: {
+    loading: true
+  },
+  parameters: {
+    ...readmeAndDesign,
+    packageName,
+    controls: {
+      exclude: ['width', 'height', 'borderRadius', 'className']
+    }
+  }
+}`,...(v=(S=c.parameters)==null?void 0:S.docs)==null?void 0:v.source}}};export{ae as default,a as skeleton,o as skeletonText,c as withSkeleton};
