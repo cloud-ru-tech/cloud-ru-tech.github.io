dddbd76fdc28e0479429aa4c6ad6c29810811d2c
cloud-ru-tech-robot <cloud-ru-tech-robot>
2026-06-24 08:45:36 +0000

    new storybook version

---
+++ snack-uikit/assets/AccordionSecondary.story-CztgbMch.js
@@ -0,0 +1,18 @@
+import{j as o}from"./jsx-runtime-BTJTZTIL.js";import{r as p}from"./index-CJ_LmFaV.js";import{c as a,a as g,b as l,d as t,s as S,S as f,C as x,D as E}from"./helperComponents-C2GkGDfm.js";import{S as u}from"./constants-BqAt9wbw.js";import"./index-ChsGqxH_.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./index-CXM-PPQz.js";import"./hooks-C5DVcyBY.js";import"./index-JZGyZbA2.js";import"./constants-BObEONKm.js";import"./componentPropsProcessors-UJEZy8aQ.js";import"./index-D3kvmOHM.js";import"./ButtonFunction-DXSJzP7S.js";import"./utils-Bd0G2N0B.js";import"./Counter-B1ZrBHB5.js";import"./Sun-BMXO82GS.js";import"./constants-BAH7I9kc.js";import"./index-DreyRvUE.js";import"./Typography-C1_yfu-P.js";import"./TruncateString-SD57muw1.js";import"./useIsomorphicLayoutEffect-BAXQmlEq.js";import"./isBrowser-C2N6XvuH.js";import"./Tooltip-DSIXDjBr.js";import"./PopoverPrivate-BoGrpd0w.js";import"./index-D8ciwPN-.js";import"./index-BRq5TIsn.js";import"./QuestionTooltip-DRd6iS0E.js";import"./index-DBTBMFOQ.js";import"./FieldText-a6Vrgh_y.js";import"./index-B7wZyJkK.js";import"./constants-9uwKbtvo.js";import"./FieldDecorator-BJDdOzik.js";import"./index-CAfvFIBF.js";import"./DropList-BTxQaH0t.js";import"./hooks-T4orCNpK.js";import"./useValueControl-CNLw7ht-.js";import"./Checkbox-S6x_jEFJ.js";import"./TogglePrivate-aVh1Iudt.js";import"./index-w0WAZL2A.js";import"./index-Y3TRpJ-L.js";import"./Switch-s0UnJpPE.js";import"./index-DHAmGKAZ.js";import"./Dropdown-Cp7_IPh2.js";import"./index-C3mvkShn.js";import"./index-CXJdfWGn.js";import"./LocaleProvider-DoPVyUfh.js";import"./index-BjbPeXNt.js";import"./IconPredefined-BFl5DzK0.js";import"./ButtonTonal-BtUAW-dk.js";import"./ButtonFilled-3I0Fv0_B.js";import"./Divider-BbKtI3tl.js";import"./Scroll-B_O1f2D1.js";import"./Spinner-kGs9Wy83.js";import"./useButtonNavigation-DalNY1jX.js";import"./useClearButton-nDBapZG3.js";import"./InputPrivate-1DbYvNBP.js";import"./getArrowIcon-Cs5vBdSw.js";import"./useValueControl-C51frgGe.js";import"./useCopyButton-BBm4LaWi.js";import"./index-DTH25FFD.js";import"./index-BGTKE3tV.js";import"./usePrefix-CWfsp2Rm.js";import"./FieldContainerPrivate-Do96kWYa.js";import"./Avatar-CkEZb7nh.js";import"./StatusIndicator-BFjfxMzR.js";const Do={title:"Components/Accordion/AccordionSecondary",component:t},k=({selectionMode:m})=>{const[d,i]=p.useState(void 0);return p.useEffect(()=>{i(void 0)},[m]),o.jsx("div",{className:S.wrapper,children:o.jsx(t,{expanded:d,onExpandedChange:i,selectionMode:m,children:Array.from({length:2}).map((h,e)=>o.jsx(t.CollapseBlock,{id:String(e),header:o.jsx(t.CollapseBlockHeader,{...E}),"data-test-id":f[e],children:o.jsx(x,{})},e))})})},r={render:k,args:{selectionMode:u.Single},argTypes:{},parameters:{readme:{sidebar:[`Latest version: ${a.version}`,g,l]},packageName:a.name,design:{name:"Figma",type:"figma",url:"https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A262646&mode=design"}}};var n,s,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: Template,
+  args: {
+    selectionMode: SELECTION_MODE.Single
+  },
+  argTypes: {},
+  parameters: {
+    readme: {
+      sidebar: [\`Latest version: \${componentPackage.version}\`, componentReadme, componentChangelog]
+    },
+    packageName: componentPackage.name,
+    design: {
+      name: 'Figma',
+      type: 'figma',
+      url: 'https://www.figma.com/file/jtGxAPvFJOMir7V0eQFukN/Snack-UI-Kit-1.1.0?node-id=41%3A262646&mode=design'
+    }
+  }
+}`,...(c=(s=r.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};export{r as accordionSecondary,Do as default};
